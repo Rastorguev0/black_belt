6a2125dc6311415a7f4c7d15a378554c752970ff
Alexandr <rastor2001@gmail.com>
2021-02-24 23:02:57 +0400

    mython lexer correctly works. passed!

--- black_belt/mython/lexer.cpp
+++ black_belt/mython/lexer.cpp
@@ -2,81 +2,242 @@
 
 #include <algorithm>
 #include <charconv>
-#include <unordered_map>
 
 using namespace std;
 
 namespace Parse {
 
-bool operator == (const Token& lhs, const Token& rhs) {
-  using namespace TokenType;
+  bool operator == (const Token& lhs, const Token& rhs) {
+    using namespace TokenType;
 
-  if (lhs.index() != rhs.index()) {
-    return false;
+    if (lhs.index() != rhs.index()) {
+      return false;
+    }
+    if (lhs.Is<Char>()) {
+      return lhs.As<Char>().value == rhs.As<Char>().value;
+    }
+    else if (lhs.Is<Number>()) {
+      return lhs.As<Number>().value == rhs.As<Number>().value;
+    }
+    else if (lhs.Is<String>()) {
+      return lhs.As<String>().value == rhs.As<String>().value;
+    }
+    else if (lhs.Is<Id>()) {
+      return lhs.As<Id>().value == rhs.As<Id>().value;
+    }
+    else {
+      return true;
+    }
   }
-  if (lhs.Is<Char>()) {
-    return lhs.As<Char>().value == rhs.As<Char>().value;
-  } else if (lhs.Is<Number>()) {
-    return lhs.As<Number>().value == rhs.As<Number>().value;
-  } else if (lhs.Is<String>()) {
-    return lhs.As<String>().value == rhs.As<String>().value;
-  } else if (lhs.Is<Id>()) {
-    return lhs.As<Id>().value == rhs.As<Id>().value;
-  } else {
-    return true;
-  }
-}
 
-std::ostream& operator << (std::ostream& os, const Token& rhs) {
-  using namespace TokenType;
+  ostream& operator << (ostream& os, const Token& rhs) {
+    using namespace TokenType;
 
 #define VALUED_OUTPUT(type) \
   if (auto p = rhs.TryAs<type>()) return os << #type << '{' << p->value << '}';
 
-  VALUED_OUTPUT(Number);
-  VALUED_OUTPUT(Id);
-  VALUED_OUTPUT(String);
-  VALUED_OUTPUT(Char);
+    VALUED_OUTPUT(Number);
+    VALUED_OUTPUT(Id);
+    VALUED_OUTPUT(String);
+    VALUED_OUTPUT(Char);
 
 #undef VALUED_OUTPUT
 
 #define UNVALUED_OUTPUT(type) \
     if (rhs.Is<type>()) return os << #type;
 
-  UNVALUED_OUTPUT(Class);
-  UNVALUED_OUTPUT(Return);
-  UNVALUED_OUTPUT(If);
-  UNVALUED_OUTPUT(Else);
-  UNVALUED_OUTPUT(Def);
-  UNVALUED_OUTPUT(Newline);
-  UNVALUED_OUTPUT(Print);
-  UNVALUED_OUTPUT(Indent);
-  UNVALUED_OUTPUT(Dedent);
-  UNVALUED_OUTPUT(And);
-  UNVALUED_OUTPUT(Or);
-  UNVALUED_OUTPUT(Not);
-  UNVALUED_OUTPUT(Eq);
-  UNVALUED_OUTPUT(NotEq);
-  UNVALUED_OUTPUT(LessOrEq);
-  UNVALUED_OUTPUT(GreaterOrEq);
-  UNVALUED_OUTPUT(None);
-  UNVALUED_OUTPUT(True);
-  UNVALUED_OUTPUT(False);
-  UNVALUED_OUTPUT(Eof);
+    UNVALUED_OUTPUT(Class);
+    UNVALUED_OUTPUT(Return);
+    UNVALUED_OUTPUT(If);
+    UNVALUED_OUTPUT(Else);
+    UNVALUED_OUTPUT(Def);
+    UNVALUED_OUTPUT(Newline);
+    UNVALUED_OUTPUT(Print);
+    UNVALUED_OUTPUT(Indent);
+    UNVALUED_OUTPUT(Dedent);
+    UNVALUED_OUTPUT(And);
+    UNVALUED_OUTPUT(Or);
+    UNVALUED_OUTPUT(Not);
+    UNVALUED_OUTPUT(Eq);
+    UNVALUED_OUTPUT(NotEq);
+    UNVALUED_OUTPUT(LessOrEq);
+    UNVALUED_OUTPUT(GreaterOrEq);
+    UNVALUED_OUTPUT(None);
+    UNVALUED_OUTPUT(True);
+    UNVALUED_OUTPUT(False);
+    UNVALUED_OUTPUT(Eof);
 
 #undef UNVALUED_OUTPUT
 
-  return os << "Unknown token :(";
-}
+    return os << "Unknown token :(";
+  }
 
 
-Lexer::Lexer(std::istream& input) {
-}
+  Lexer::Lexer(istream& input) : input(input) {
+    while (input.peek() == '\n') {
+      input.ignore(1);
+    }
+    NextToken();
+  }
 
-const Token& Lexer::CurrentToken() const {
-}
+  const Token& Lexer::CurrentToken() const {
+    return current_token;
+  }
 
-Token Lexer::NextToken() {
-}
+  Token Lexer::NextToken() {
+    optional<Token> token = TryEof();
+
+    SkipEmptyLines();
+    if (!token) {
+      token = ControlIndent();
+    }
+    SkipSpaces(input);
+
+    if (!token) {
+      token = TryNumber();
+    }
+    if (!token) {
+      token = TryAnyId();
+    }
+    if (!token) {
+      token = TryString();
+    }
+    if (!token) {
+      token = TryChar();
+    }
+    current_token = token.value();
+    return current_token;
+  }
+
+  optional<Token> Lexer::ControlIndent() {
+    if (unreturned_dif > 0) {
+      unreturned_dif--;
+      if (current_token.Is<TokenType::Indent>()) indent++;
+      else indent--;
+      return current_token;
+    }
+    if (!current_token.Is<TokenType::Newline>()) return nullopt;
+    char space;
+    unsigned cur_indent = 0;
+    for (int i = 0; input.get(space); ++i) {
+      if (space == ' ') {
+        cur_indent += i % 2;
+      }
+      else {
+        input.putback(space);
+        break;
+      }
+    }
+    unreturned_dif = max(cur_indent, indent) - min(cur_indent, indent);
+    if (cur_indent > indent) {
+      unreturned_dif--;
+      indent++;
+      return TokenType::Indent{};
+    }
+    else if (cur_indent < indent) {
+      indent--;
+      unreturned_dif--;
+      return TokenType::Dedent{};
+    }
+    else return nullopt;
+  }
+
+  optional<Token> Lexer::TryNumber() {
+    if (IsNumber(input.peek())) {
+      int n;
+      input >> n;
+      return TokenType::Number{ n };
+    }
+    else return nullopt;
+  }
+
+  optional<Token> Lexer::TryAnyId() {
+    string id;
+    char another;
+    while (input.get(another) && IdChar(another)) {
+      id.push_back(another);
+    }
+    if (input) input.putback(another);
+    if (translator.count(id)) return translator.at(id);
+    else if (!id.empty()) return TokenType::Id{ id };
+    else return nullopt;
+  }
+
+
+  optional<Token> Lexer::TryString() {
+    string str;
+    char quote = input.get();
+    if (quote == '\'' || quote == '\"') {
+      getline(input, str, quote);
+      return TokenType::String{ str };
+    }
+    else input.putback(quote);
+    return nullopt;
+  }
+
+
+  optional<Token> Lexer::TryChar() {
+    char c = input.get();
+    if (c == '\n') return TokenType::Newline{};
+    else if (c == '!' && input.peek() == '=') {
+      input.ignore(1);
+      return TokenType::NotEq{};
+    }
+    else if (c == '<' && input.peek() == '=') {
+      input.ignore(1);
+      return TokenType::LessOrEq{};
+    }
+    else if (c == '>' && input.peek() == '=') {
+      input.ignore(1);
+      return TokenType::GreaterOrEq{};
+    }
+    else if (c == '=' && input.peek() == '=') {
+      input.ignore(1);
+      return TokenType::Eq{};
+    }
+    return TokenType::Char{ c };
+  }
+
+  optional<Token> Lexer::TryEof() {
+    if (current_token.Is<TokenType::Eof>()) return current_token;
+    if (char _;  input && input.get(_)) {
+      input.putback(_);
+      return nullopt;
+    }
+    else {
+      if (!current_token.Is<TokenType::Newline>() && !current_token.Is<TokenType::Dedent>()) {
+        return TokenType::Newline{};
+      }
+      else {
+        if (indent > 0) {
+          indent--;
+          return TokenType::Dedent{};
+        }
+        else return TokenType::Eof{};
+      }
+    }
+  }
+
+
+
+  bool IsNumber(char c) {
+    return ('0' <= c && c <= '9');
+  }
+
+  bool IdChar(char c) {
+    return ('A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || c == '_' || IsNumber(c));
+  }
+
+  void SkipSpaces(istream& input) {
+    while (input.peek() == ' ') {
+      input.ignore(1);
+    }
+  }
+
+  void Lexer::SkipEmptyLines() {
+    while (current_token.Is<TokenType::Newline>() && input.peek() == '\n') {
+      input.ignore(1);
+    }
+  }
 
 } /* namespace Parse */
--- black_belt/mython/lexer.h
+++ black_belt/mython/lexer.h
@@ -6,139 +6,180 @@
 #include <variant>
 #include <stdexcept>
 #include <optional>
+#include <unordered_map>
 
 class TestRunner;
 
 namespace Parse {
 
-namespace TokenType {
-  struct Number {
-    int value;
+  namespace TokenType {
+    struct Number {
+      int value;
+    };
+
+    struct Id {
+      std::string value;
+    };
+
+    struct Char {
+      char value;
+    };
+
+    struct String {
+      std::string value;
+    };
+
+    struct Class {};
+    struct Return {};
+    struct If {};
+    struct Else {};
+    struct Def {};
+    struct Newline {};
+    struct Print {};
+    struct Indent {};
+    struct Dedent {};
+    struct Eof {};
+    struct And {};
+    struct Or {};
+    struct Not {};
+    struct Eq {};
+    struct NotEq {};
+    struct LessOrEq {};
+    struct GreaterOrEq {};
+    struct None {};
+    struct True {};
+    struct False {};
+  }
+
+  using TokenBase = std::variant <
+    TokenType::Number,
+    TokenType::Id,
+    TokenType::Char,
+    TokenType::String,
+    TokenType::Class,
+    TokenType::Return,
+    TokenType::If,
+    TokenType::Else,
+    TokenType::Def,
+    TokenType::Newline,
+    TokenType::Print,
+    TokenType::Indent,
+    TokenType::Dedent,
+    TokenType::And,
+    TokenType::Or,
+    TokenType::Not,
+    TokenType::Eq,
+    TokenType::NotEq,
+    TokenType::LessOrEq,
+    TokenType::GreaterOrEq,
+    TokenType::None,
+    TokenType::True,
+    TokenType::False,
+    TokenType::Eof
+  > ;
+
+
+  //По сравнению с условием задачи мы добавили в тип Token несколько
+  //удобных методов, которые делают код короче. Например,
+  //
+  //token.Is<TokenType::Id>()
+  //
+  //гораздо короче, чем
+  //
+  //std::holds_alternative<TokenType::Id>(token).
+  struct Token : TokenBase {
+    using TokenBase::TokenBase;
+
+    template <typename T>
+    bool Is() const {
+      return std::holds_alternative<T>(*this);
+    }
+
+    template <typename T>
+    const T& As() const {
+      return std::get<T>(*this);
+    }
+
+    template <typename T>
+    const T* TryAs() const {
+      return std::get_if<T>(this);
+    }
   };
 
-  struct Id {
-    std::string value;
+  bool operator == (const Token& lhs, const Token& rhs);
+  std::ostream& operator << (std::ostream& os, const Token& rhs);
+
+  class LexerError : public std::runtime_error {
+  public:
+    using std::runtime_error::runtime_error;
   };
 
-  struct Char {
-    char value;
+  class Lexer {
+  public:
+    explicit Lexer(std::istream& input);
+
+    const Token& CurrentToken() const;
+    Token NextToken();
+
+    template <typename T>
+    const T& Expect() const {
+      if (!current_token.Is<T>()) {
+        throw LexerError("");
+      }
+      else return *current_token.TryAs<T>();
+    }
+
+    template <typename T, typename U>
+    void Expect(const U& value) const {
+      if (!(current_token.Is<T>() && current_token.TryAs<T>()->value == value)) {
+        throw LexerError("");
+      }
+    }
+
+    template <typename T>
+    const T& ExpectNext() {
+      NextToken();
+      return Expect<T>();
+    }
+
+    template <typename T, typename U>
+    void ExpectNext(const U& value) {
+      NextToken();
+      Expect<T>(value);
+    }
+  private:
+    void SkipEmptyLines();
+    std::optional<Token> ControlIndent();
+    std::optional<Token> TryNumber();
+    std::optional<Token> TryAnyId();
+    std::optional<Token> TryChar();
+    std::optional<Token> TryString();
+    std::optional<Token> TryEof();
+  private:
+    const std::unordered_map<std::string, Token> translator = {
+      {"class", TokenType::Class{}},
+      {"return", TokenType::Return{}},
+      {"if", TokenType::If{}},
+      {"else", TokenType::Else{}},
+      {"def", TokenType::Def{}},
+      {"print", TokenType::Print{}},
+      {"and", TokenType::And{}},
+      {"or", TokenType::Or{}},
+      {"not", TokenType::Not{}},
+      {"None", TokenType::None{}},
+      {"True", TokenType::True{}},
+      {"False", TokenType::False{}}
+    };
+  private:
+    std::istream& input;
+    Token current_token;
+    unsigned indent = 0;
+    unsigned unreturned_dif = 0;
   };
 
-  struct String {
-    std::string value;
-  };
-
-  struct Class{};
-  struct Return{};
-  struct If {};
-  struct Else {};
-  struct Def {};
-  struct Newline {};
-  struct Print {};
-  struct Indent {};
-  struct Dedent {};
-  struct Eof {};
-  struct And {};
-  struct Or {};
-  struct Not {};
-  struct Eq {};
-  struct NotEq {};
-  struct LessOrEq {};
-  struct GreaterOrEq {};
-  struct None {};
-  struct True {};
-  struct False {};
-}
-
-using TokenBase = std::variant<
-  TokenType::Number,
-  TokenType::Id,
-  TokenType::Char,
-  TokenType::String,
-  TokenType::Class,
-  TokenType::Return,
-  TokenType::If,
-  TokenType::Else,
-  TokenType::Def,
-  TokenType::Newline,
-  TokenType::Print,
-  TokenType::Indent,
-  TokenType::Dedent,
-  TokenType::And,
-  TokenType::Or,
-  TokenType::Not,
-  TokenType::Eq,
-  TokenType::NotEq,
-  TokenType::LessOrEq,
-  TokenType::GreaterOrEq,
-  TokenType::None,
-  TokenType::True,
-  TokenType::False,
-  TokenType::Eof
->;
-
-
-//По сравнению с условием задачи мы добавили в тип Token несколько
-//удобных методов, которые делают код короче. Например,
-//
-//token.Is<TokenType::Id>()
-//
-//гораздо короче, чем
-//
-//std::holds_alternative<TokenType::Id>(token).
-struct Token : TokenBase {
-  using TokenBase::TokenBase;
-
-  template <typename T>
-  bool Is() const {
-    return std::holds_alternative<T>(*this);
-  }
-
-  template <typename T>
-  const T& As() const {
-    return std::get<T>(*this);
-  }
-
-  template <typename T>
-  const T* TryAs() const {
-    return std::get_if<T>(this);
-  }
-};
-
-bool operator == (const Token& lhs, const Token& rhs);
-std::ostream& operator << (std::ostream& os, const Token& rhs);
-
-class LexerError : public std::runtime_error {
-public:
-  using std::runtime_error::runtime_error;
-};
-
-class Lexer {
-public:
-  explicit Lexer(std::istream& input);
-
-  const Token& CurrentToken() const;
-  Token NextToken();
-
-  template <typename T>
-  const T& Expect() const {
-  }
-
-  template <typename T, typename U>
-  void Expect(const U& value) const {
-  }
-
-  template <typename T>
-  const T& ExpectNext() {
-  }
-
-  template <typename T, typename U>
-  void ExpectNext(const U& value) {
-  }
-
-private:
-};
-
-void RunLexerTests(TestRunner& test_runner);
+  void RunLexerTests(TestRunner& test_runner);
+  bool IsNumber(char c);
+  bool IdChar(char c);
+  void SkipSpaces(std::istream& in);
 
 } /* namespace Parse */

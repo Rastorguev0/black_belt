9f4d6d1255848b28c53a84f920635946441905cc
Alexandr <rastor2001@gmail.com>
2021-03-22 21:27:36 +0300

    my optional done

--- black_belt/optional/optional.h
+++ black_belt/optional/optional.h
@@ -1,3 +1,5 @@
+#include <utility>
+
 // Исключение этого типа должно генерироваться при обращении к "пустому" Optional в функции Value
 struct BadOptionalAccess {
 };
@@ -41,86 +43,129 @@ public:
 
 template<typename T>
 Optional<T>::Optional(const T& elem) {
-  T* buff = new(data) T;
-  *buff = elem;
+  new (data) T(elem);
+  defined = true;
 }
 
 template<typename T>
 Optional<T>::Optional(T&& elem) {
-
+  new (data) T(std::move(elem));
+  defined = true;
 }
 
 template<typename T>
 Optional<T>::Optional(const Optional& other) {
-
+  *this = other;
 }
 
 template<typename T>
 Optional<T>::Optional(Optional&& other) {
-
+  *this = std::move(other);
 }
 
 template<typename T>
 Optional<T>& Optional<T>::operator=(const T& elem) {
-
+  if (defined) {
+    (*this).operator*() = elem;
+  }
+  else {
+    new (data) T(elem);
+    defined = true;
+  }
+  return *this;
 }
 
 template<typename T>
 Optional<T>& Optional<T>::operator=(T&& elem) {
-
+  if (defined) {
+    (*this).operator*() = elem;
+  }
+  else {
+    new (data) T(std::move(elem));
+    defined = true;
+  }
+  return *this;
 }
 
 template<typename T>
 Optional<T>& Optional<T>::operator=(const Optional& other) {
-
+  if (defined && other.defined) {
+    (*this).operator*() = *other;
+  }
+  else if (defined) {
+    (*this).operator*().~T();
+  }
+  else if (other.defined) {
+    new (data) T{*other};
+  }
+  defined = other.defined;
+  return *this;
 }
 
 template<typename T>
 Optional<T>& Optional<T>::operator=(Optional&& other) {
-
+  if (defined && other.defined) {
+    (*this).operator*() = std::move(*other);
+  }
+  else if (defined) {
+    (*this).operator*().~T();
+  }
+  else if (other.defined) {
+    new (data) T{std::move(*other)};
+  }
+  defined = other.defined;
+  return *this;
 }
 
 template<typename T>
 bool Optional<T>::HasValue() const {
-
+  return defined;
 }
 
 template<typename T>
 T& Optional<T>::operator*() {
-
+  return *reinterpret_cast<T*>(&data);
 }
 
 template<typename T>
 const T& Optional<T>::operator*() const {
-
+  return *reinterpret_cast<const T*>(&data);
 }
 
 template<typename T>
 T* Optional<T>::operator->() {
-
+  return reinterpret_cast<T*>(&data);
 }
 
 template<typename T>
 const T* Optional<T>::operator->() const {
-
+  return reinterpret_cast<const T*>(&data);
 }
 
 template<typename T>
 T& Optional<T>::Value() {
-
+  if (!defined) throw BadOptionalAccess{};
+  return *reinterpret_cast<T*>(&data);
 }
 
 template<typename T>
 const T& Optional<T>::Value() const {
-
+  if (!defined) throw BadOptionalAccess{};
+  return *reinterpret_cast<const T*>(&data);
 }
 
 template<typename T>
 void Optional<T>::Reset() {
-
+  if (defined) {
+    reinterpret_cast<T*>(&data)->~T();
+  }
+  defined = false;
 }
 
 template<typename T>
 Optional<T>::~Optional() {
-  
+  if (defined) {
+    (*this).operator*().~T();
+  }
+  defined = false;
 }
